4e02f11577163b4ff9b472026966b4c1ba9cd0b6
Hemanth <hemanth.kotagiri43@gmail.com>
2022-01-14 15:16:52 +0530

    Add AllResultsWidget.dart

---
+++ lib/Widgets/AllResultsWidget.dart
@@ -0,0 +1,96 @@
+import 'package:flutter/material.dart';
+
+import 'NewResultsFetcherWidget.dart';
+
+class AllResultsList extends StatefulWidget {
+  final data;
+  const AllResultsList({Key key, @required this.data}) : super(key: key);
+
+  @override
+  _AllResultsListState createState() => _AllResultsListState();
+}
+
+class _AllResultsListState extends State<AllResultsList> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Expanded(
+          child: ListView.builder(
+            itemCount: widget.data.length,
+            itemBuilder: (context, index) {
+              final data = widget.data[index];
+              return Column(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: GestureDetector(
+                      onTap: () => {_individualResultPage(data)},
+                      child: resultCard(data),
+                    ),
+                  ),
+                  Divider(thickness: 1, endIndent: 50, indent: 50),
+                ],
+              );
+            },
+          ),
+        ),
+      ],
+    );
+  }
+
+  Center getDate(data) {
+    return Center(
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Text(
+          data.values.elementAt(1), // Date
+          style: TextStyle(
+            color: Colors.green,
+            fontWeight: FontWeight.w500,
+          ),
+        ),
+      ),
+    );
+  }
+
+  ListTile getSupplyExamName(data) {
+    return ListTile(
+      title: Text(data.values.elementAt(0), // Exam Name
+          textAlign: TextAlign.center,
+          style: TextStyle(
+            color: Colors.blue[200],
+          )),
+    );
+  }
+
+  Card resultCard(data) {
+    return Card(
+      child: Column(
+        children: [
+          getDate(data),
+          getSupplyExamName(data),
+        ],
+      ),
+    );
+  }
+
+  _individualResultPage(Map<String, dynamic> data) {
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (BuildContext context) {
+          return Scaffold(
+            appBar: AppBar(
+              title: Text(
+                "Supplementary Results",
+                style: TextStyle(color: Colors.cyan),
+              ),
+              centerTitle: true,
+            ),
+            body: NewResultsFetcherWidget(data: data),
+          );
+        },
+      ),
+    );
+  }
+}
